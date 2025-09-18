b316d2a1f7b1ee97225881def2d48dd8160b097d
bogar <bogar@gmail.com>
2025-09-18 09:34:40 +0530

    ch

--- src/routes/allRoutes.tsx
+++ src/routes/allRoutes.tsx
@@ -5,25 +5,13 @@ const authProtectedRoutes = [
     path: "/",
     component: <Dashboard />,
   },
-//   {
-//     path: "/changePassword",
-//     component: <ChangePassword />,
-//   },
-//   { path: "/patient", component: <Patient /> },
-//   { path: "/patient/:pid", component: <Patient /> },
-//   { path: "/PatientTable", component: <Patientindex /> },
-//   { path: "/orgList", component: <Organizationindex /> },
-//   { path: "/orgRegister", component: <OrganizationRegister /> },
-//   { path: "/orgRegister/:orgId", component: <OrganizationRegister /> },
-//   { path: "/staffIndex", component: <StaffIndex /> },
-//   { path: "/staffRegister", component: <StaffRegister /> },
-//   { path: "/staffRegister/:staffId", component: <StaffRegister /> },
-//   { path: "/physicianIndex", component: <PhysicianIndex /> },
-//   { path: "/physicianRegister", component: <PhysicianRegister /> },
-//   { path: "/physicianRegister/:phyId", component: <PhysicianRegister /> },
 ];
-const publicRoutes = [
-//   { path: "/", component: <LoginIndex /> },
-//   { path: "/forgotpassword", component: <Forgotpassword /> },
-];
+
+const publicRoutes: any[] = [
+    {
+    path: "/",
+    component: <Dashboard />,
+  },
+]; // can add login/signup later
+
 export { authProtectedRoutes, publicRoutes };
